bfacc9c3d24153cd82635da70a4786cf043549d2
Ayush Agrawal <41867842+mrayushagrawal0210@users.noreply.github.com>
2021-10-09 17:17:28 +0530

    Update c.c
    This program will print Hello world 3 sec

--- languages/c.c
+++ languages/c.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include <time.h>
 
-int main(){
-
-printf("Hi!");
-
-return 0;
+int main(void)
+{
+    time_t start, end;
+    double elapsed, prev_elapsed = 0.0;
+    time(&start);  /* start the timer */
+    do
+    {
+        time(&end);
+        elapsed = difftime(end, start);
+        if (elapsed >= prev_elapsed+3.0)// the time interval of 3.0 sec 
+        {
+            printf("Hello World!\n");
+            prev_elapsed = elapsed;
+        }
+    } while(elapsed < 4.0);
 }
